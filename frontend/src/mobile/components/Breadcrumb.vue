9345c289e3c8ca682402b4db35828f375dd639d9
jimwu979 <jimwu979@gmail.com>
2026-07-31 13:43:08 +0800

    [Breadcrumb] add settings breadcrumb component
    - 新增可傳入連結與文字的麵包屑元件
    - 完成分隔符號與目前項目樣式

---
+++ frontend/src/mobile/components/Breadcrumb.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="component breadcrumb">
+    <template v-for="(item, index) in items" :key="index">
+      <router-link :to="item.url">
+        {{ item.text }}
+      </router-link>
+    </template>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  items: {
+    url: string
+    text: string
+  }[]
+}>()
+</script>
+
+<style lang="scss" scoped>
+.component.breadcrumb {
+  >a{
+    color: $brown;
+    @include h3();
+    text-underline-offset: 3px;
+    text-decoration-color: $brown;
+    text-decoration-line: underline;
+    ~a{
+      margin-left: 20px;
+      position: relative;
+      &:before{
+        left: -20px;
+        width: 20px;
+        content: '/';
+        @include h3();
+        position: absolute;
+        text-align: center;
+        display: inline-block;
+      }
+    }
+    &:nth-last-of-type(1){
+      color: $grey;
+      pointer-events: none;
+      text-decoration-line: none;
+    }
+  }
+}
+</style>
